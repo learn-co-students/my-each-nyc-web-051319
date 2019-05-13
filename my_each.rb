847bc47f5c34321e09a59c5307e574be7c2589a7
Raphael Saphra <dsaphra@Macintosh-c82a1445a595.local>
2019-05-13 08:33:32 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+
+def get_length(collection)
+  collection.length
+end
+
+def my_each(collection)
   # code here
+  if block_given?
+    i = 0
+    collection_length = get_length(collection)
+    while i < collection_length
+      yield(collection[i])
+      i += 1
+    end
+  end
+  collection
 end
